738be496b28a01c668f433d2bc0f0e7a8e6e7252
Akshata Bihany <akshatabihany@gmail.com>
2020-04-30 01:08:40 +0530

    10

--- server.js
+++ server.js
@@ -21,18 +21,41 @@ app.post('/display',(req,res) => {
         if (err) throw err
         let dbll = db.db("atom")
         let query = {
-            name : req.body.name
+            name : req.body.name,
         }
         dbll.collection('users').findOne(query ,(dbErr,result) => {
             if(dbErr) throw dbErr
             console.log(result)
             if(result){ 
-                res.render('server',{name : result.name})
+                res.render('server',{name : result._id})
             } else {
                 res.render('server',{name:'not found'})
             }
         })
     })
 })
+
+app.post('/display',(req,res) => {
+
+    MongoClient.connect(uri ,{useUnifiedTopology:true},(err,db) => {
+        if (err) throw err
+        let dbll = db.db("atom")
+        dbll.tasks,user .find({
+            "$where": "this. tasks.members._id  == this.user._id"
+        },{
+            "task.title": 1,
+            "": 1
+        }
+        );
+        if(dbErr) throw dbErr
+        console.log(result)
+        if(result){ 
+            res.render('server',{title : result})
+        } else {
+            res.render('server',{title:'not found'})
+        }
+        })
+    })
+
 
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
