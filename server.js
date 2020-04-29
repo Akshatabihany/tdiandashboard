b94d3107bc3befad5b170178385d5b31d645503d
Arindam Lahiri <arindamlahiri17@gmail.com>
2020-04-29 21:51:43 +0530

    changes by arindam

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
 const express = require('express');
 const app = express();
 const mongodb = require('mongodb');
-const bodyParser = require('body-parser');
 
 const port=3000;
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 
 app.set('view engine', 'ejs')
 
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = mongodb.MongoClient
 const uri = "mongodb+srv://atom:atom@cluster0-5i0bk.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-app.get('/display',(req,res)=>
-{
-    res.sendFile(__dirname+'/server.html')
-}
-)
-app.get('/display',(req,res) => {
 
-    MongoClient.connect(uri ,(err,db) => {
+
+app.get('/display',(req,res)=>{
+    res.render('server',{name:""})
+})
+app.post('/display',(req,res) => {
+
+    MongoClient.connect(uri ,{useUnifiedTopology:true},(err,db) => {
         if (err) throw err
         let dbll = db.db("atom")
         let query = {
             name : req.body.name
         }
-        dbll.collection('users').find(query ,(dbErr,result) => {
+        dbll.collection('users').findOne(query ,(dbErr,result) => {
             if(dbErr) throw dbErr
-            res.render('server',{name : 'result.name'
-        })
-        console.log("ll")
-            db.close()
+            console.log(result)
+            if(result){ 
+                res.render('server',{name : result.name})
+            } else {
+                res.render('server',{name:'not found'})
+            }
         })
     })
 })
 
-var server=app.listen(port, () => console.log(`E`))
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
