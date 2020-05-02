741c2f760ab9002e6e21babedd925f1ed6653f19
Akshata Bihany <akshatabihany@gmail.com>
2020-05-03 00:11:26 +0530

    ww

--- app.js
+++ app.js
@@ -1,46 +1,26 @@
 const express = require('express');
 const app = express();
 const mongodb = require('mongodb');
-const bodyParser = require('body-parser');
-const port=3000;
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
-
+const port=5000;
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 app.set('view engine', 'ejs')
-
-const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb+srv://atom:atom@cluster0-5i0bk.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(url, { useNewUrlParser: true });
-client.connect(err => {
-    const collection = client.db("atom").collection("users");
-    console.log("sdf");
-    // perform actions on the collection object
-   client.close();
-  });
-
-app.get('/display',(req,res) => {
-    
-    MongoClient.connect(url,(err,db) => {
-        if (err) throw err
-
-        let dbo = db.db("atom")
-        let query = {}
-
-        dbo.collection("users").find(query).toArray((dbErr,result) => {
-            if(dbErr) throw dbErr
-
-            res.render('index',{'users' : result})
-
-            db.close()
-        })
-    })
+const MongoClient = mongodb.MongoClient
+const uri = "mongodb+srv://atom:atom@cluster0-5i0bk.mongodb.net/test?retryWrites=true&w=majority";
+app.get('/display',(req,res)=>{
+    res.render('server',{name:"",tasks:""})
 })
-//////app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+ app.post('/display',(req,res) => {
+     MongoClient.connect(uri ,(err,db) => {
+         
+         let dbll = db.db("atom")
+         dbll.collection('tasks').find({}).toArray(function(err, docs){
+            console.log(JSON.stringify(docs[0].members[0].id))
+           } )
+        
+    })
+ })
 
-// app.listen(2000,(req,res)=>
-//     {
-//         console.log("hh")
-//     })
-
-  
-    
+app.listen(port, (req,res) => {
+console.log(`Server running `)}
+)
--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
 const mongodb = require('mongodb');
-const port=2000;
+const port=3000;
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.set('view engine', 'ejs')
 const MongoClient = mongodb.MongoClient
 const uri = "mongodb+srv://atom:atom@cluster0-5i0bk.mongodb.net/test?retryWrites=true&w=majority";
 app.get('/display',(req,res)=>{
-    res.render('server',{name:"",tasks:""})
+    res.render('server',{name:""})
 })
+
  app.post('/display',(req,res) => {
      MongoClient.connect(uri ,(err,db) => {
          if (err) throw err
@@ -19,19 +20,34 @@ app.get('/display',(req,res)=>{
          }
          var p;
          dbll.collection('users').findOne(query ,(dbErr,result) => {
-            if(dbErr) throw dbErr
+            if(dbErr) 
+            {throw dbErr}
             else
-           { p=JSON.stringify(result._id);
-            console.log(result._id)
-             console.log(p)
-    }
-         })
-            
-          const s =  dbll.collection('tasks').find({
-            'members': { $elemMatch: { 'id': p } }
-          });
-                console.log(s.name)
-        
+            { p=JSON.stringify(result._id);
+        //      console.log(result._id)
+            //+ console.log(p)
+            }
+           })
+
+           dbll.collection('tasks').find({}).toArray(function(err, docs){
+             var i,j;
+            console.log(JSON.stringify(docs[0].members[0].id))
+            for(i in docs){
+            for(j in docs[i].members)
+            {
+             // console.log(JSON.stringify(docs[i].members[j].id))
+              const T=docs[i]
+              const t=JSON.stringify(docs[i].members[j].id)
+              if(t==p)
+              {//console.log(t)
+               console.log(T.title)
+               console.log(docs[i].members[j].name)
+             }
+            }
+
+           }
+           } )
+
     })
  })
 
