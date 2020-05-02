51996402c55ee97e3e80a812feebe5493ed613a7
Akshata Bihany <akshatabihany@gmail.com>
2020-05-03 02:32:09 +0530

    ooo

--- app.js
+++ app.js
@@ -1,23 +1,68 @@
 const express = require('express');
 const app = express();
 const mongodb = require('mongodb');
-const port=5000;
+const port=3000;
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.set('view engine', 'ejs')
 const MongoClient = mongodb.MongoClient
 const uri = "mongodb+srv://atom:atom@cluster0-5i0bk.mongodb.net/test?retryWrites=true&w=majority";
 app.get('/display',(req,res)=>{
-    res.render('server',{name:"",tasks:""})
+    res.render('server',{name:""})
 })
+
  app.post('/display',(req,res) => {
      MongoClient.connect(uri ,(err,db) => {
-         
+         if (err) throw err
          let dbll = db.db("atom")
-         dbll.collection('tasks').find({}).toArray(function(err, docs){
-            console.log(JSON.stringify(docs[0].members[0].id))
-           } )
-        
+         let query = {
+             name : req.body.name
+         }
+         var p;
+         dbll.collection('users').findOne(query ,(dbErr,result) => {
+            if(dbErr) 
+            {throw dbErr}
+            else
+            { p=JSON.stringify(result._id);
+            }
+           })
+           dbll.collection('tasks').find({}).toArray(function(err, docs){
+             var i,j;
+            for(i in docs){
+            for(j in docs[i].members)
+            {
+              const T=docs[i]
+              const t=JSON.stringify(docs[i].members[j].id)
+              const tt=docs[i].members[j].subtasks
+              if(t==p)
+              {
+               for(k in T.resources)
+               {  const res=JSON.stringify(T.resource[k])
+                 console.log(res)
+               }
+              for(k in tt)
+                {   var taskdone,nooftasks;
+                ////  console.log(tt[k].title)
+                  ////console.log(JSON.stringify(tt[k].status))
+                  const status=JSON.stringify(tt[k].status)
+                  if(status)
+                  {
+                      taskdone=taskdone+1
+                  }
+                }
+                nooftasks=tt.length
+               // console.log(taskdone)
+               //console.log(nooftasks)
+               const percentage=(taskdone/nooftasks)*100
+               ///console.log(percentage)
+               const title=T.title
+               const deadline=JSON.stringify(T.deadline)
+               const description=T.description
+               const name=docs[i].members[j].name
+             }}
+            }
+           })
+           
     })
  })
 
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
 const mongodb = require('mongodb');
-const port=3000;
+const port=2000;
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.set('view engine', 'ejs')
 const MongoClient = mongodb.MongoClient
 const uri = "mongodb+srv://atom:atom@cluster0-5i0bk.mongodb.net/test?retryWrites=true&w=majority";
 app.get('/display',(req,res)=>{
-    res.render('server',{name:""},{title:""},{deadline:""},{description:""})
+///  res.render('server',{name:""})
+ 
+    res.render('server',{name:"",title:"",deadline:"",description:"",percentage:""})
 })
 
  app.post('/display',(req,res) => {
@@ -26,34 +28,52 @@ app.get('/display',(req,res)=>{
             { p=JSON.stringify(result._id);
             }
            })
-
            dbll.collection('tasks').find({}).toArray(function(err, docs){
              var i,j;
-            console.log(JSON.stringify(docs[0].members[0].id))
+          //  console.log(JSON.stringify(docs[0].members[0].id))
             for(i in docs){
             for(j in docs[i].members)
             {
              // console.log(JSON.stringify(docs[i].members[j].id))
               const T=docs[i]
               const t=JSON.stringify(docs[i].members[j].id)
+              const tt=docs[i].members[j].subtasks
               if(t==p)
               {//console.log(t)
               //  console.log(T.title)
               //  console.log(JSON.stringify(T.deadline))
               //  console.log(T.description)
-                console.log(docs[i].members[j].subtasks)
-               const title=T.title
-               const deadline=JSON.stringify(T.deadline)
-               const description=T.description
-               const name=docs[i].members[j].name
-               
-             }
-            }
+              // resourcess
+              // for(k in T.resources)
+              // {  const res=T.resource[k]
+              //   console.log(res)
+              // }
+              for(k in tt)
+              {   var taskdone,nooftasks;
+              ////  console.log(tt[k].title)
+                ////console.log(JSON.stringify(tt[k].status))
+                const status=JSON.stringify(tt[k].status)
+                if(status)
+                {
+                    taskdone=taskdone+1
+                }
+              }
+              nooftasks=tt.length
+              console.log(taskdone)
+             console.log(nooftasks)
+             const percentage=(taskdone/nooftasks)*100
+             console.log(percentage)
+             const title=T.title
+             const deadline=JSON.stringify(T.deadline)
+             const description=T.description
+             const name=docs[i].members[j].name
+           }}
 
-           }
-           }
-           )
-           res.render('server',{name:name},{title:title},{deadline:deadline},{description:description})
+          }
+          
+        })
+       /// dbll.close()
+                    res.render('server',{name:name,title:title,deadline:deadline,description:description,percentage:percentage})
     })
  })
 
